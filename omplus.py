f4f4bc7abdc883fe63f6020beeefba31da290d45
MANISH KUMAR <manish.kumar@shaibiginfosystem.com>
2017-11-22 23:22:35 +0530

    purchase and expense Apis.

--- omplus.py
+++ omplus.py
@@ -97,7 +97,60 @@ def product_list():
     return product.get_product_list()
 
 
-# ==============================================================
+###################Manish Api########################################
+
+
+@app.route('/seller/save-purchase-detail', methods=['POST'])
+def save_purchase_detail():
+    output_data = {'error': ''}
+    purchase = db.purchase
+    supplier_id = request.values.get('supplier_id')   #supplier collection
+    product_id = request.values.get('product_id')     #product collection
+    quantity = request.values.get('quantity')         
+    unit = request.values.get('unit')
+    batch_no = request.values.get('batch_no')
+    date_of_mfg = request.values.get('date_of_mfg')
+    exp_date = request.values.get('exp_date')
+    total_cost = request.values.get('total_cost')
+    unit_cost = request.values.get('unit_cost')
+    invoice_no = request.values.get('invoice_no')
+    tax_id = request.values.get('tax_id')                #tax collection
+    date_of_receiving = request.values.get('date_of_receiving') 
+    warehouse_id = request.values.get('warehouse_id')     #warehouse collection
+    purchase_id = purchase.insert({'supplier':supplier_id,'product':product_id,
+                                'quantity':quantity,'unit':unit,
+                                'batch_no':batch_no,'date_of_mfg':date_of_mfg,
+                                'exp_date':exp_date,'total_cost':total_cost,
+                                'unit_cost':unit_cost,'invoice_no':invoice_no,
+                                'tax':tax_id,'date_of_receiving':date_of_receiving,
+                                 'warehouse':warehouse_id})
+    output_data['result'] = 'Purchase details successfully saved'
+    return jsonify(output_data)
+
+
+@app.route('/seller/save-expense-detail', methods=['POST'])
+def save_expens_detail():
+    output_data = {'error': ''}
+    expense = db.expense
+    name = request.values.get('name')  
+    org_id = request.values.get('org_id')  #org collection
+    # gst_no = request.values.get('gst_no')         
+    total_cost = request.values.get('total_cost')
+    tax_paid = request.values.get('tax_paid')
+    tax_id = request.values.get('tax_id')   #tax collection
+    total_amount_with_tax = int(total_cost) + int(tax_paid)
+    invoice_no = request.values.get('invoice_no')
+    date = request.values.get('date')
+    expense_id = expense.insert({'name':name,'org':org_id,
+                                'total_cost':total_cost,'tax_paid':tax_paid,
+                                'tax':tax_id,'total_amount_with_tax':total_amount_with_tax,
+                                'invoice_no':invoice_no,'date':date
+                                })
+    output_data['result'] = 'Expense details successfully saved'
+    return jsonify(output_data)
+
+
+# =======================================================================
 
 
 if __name__ == '__main__':
