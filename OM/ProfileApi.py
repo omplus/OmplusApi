a1e75c02c8241dd7727072ca9a67540c54c9ca3f
cp <chandra.pkash@gmail.com>
2017-11-28 23:22:17 +0530

    Update

--- OM/ProfileApi.py
+++ OM/ProfileApi.py
@@ -1,6 +1,7 @@
 from flask import jsonify
-from flask import Flask, request, flash, url_for, redirect, render_template
+from flask import Flask, request,session, flash, url_for, redirect, render_template
 from flask_pymongo import PyMongo
+from werkzeug import generate_password_hash,check_password_hash
 
 app = Flask(__name__)
 
@@ -10,98 +11,117 @@ app.config['MONGO_URI'] = 'mongodb://localhost:27017/omplus'
 mongo = PyMongo(app)
 with app.app_context():
     db = mongo.db
+ERROR_NotJson = "Request Payload is Not JSon"
 
-#register
 
+# user--------------
 @app.route('/api/user/register', methods=['POST'])
 def register():
-    reg = mongo.db.user_details
+    output_data = {'error': ''}
+    if not request.json:
+        output_data['error'] = ERROR_NotJson
+        return jsonify(output_data)
 
     sellerId = request.json['sellerId']
     firstName = request.json['firstName']
     lastName = request.json['lastName']
-    email = request.json['email']
+    emailid = request.json['email']
     phoneNo = request.json['phoneNo']
     pwd = request.json['pwd']
     confirmPwd = request.json['confirmPwd']
-
-    user = reg.insert({'sellerId': sellerId, 'firstName': firstName, 'lastName': lastName,
-                       'email': email, 'phoneNo': phoneNo, 'pwd': pwd, 'confirmPwd': confirmPwd})
-    data = reg.find_one({"_id": user})
-
-    return jsonify({"data": data})
-
-#get all users
-
-@app.route('/api/getAllUsers', methods=['GET'])
-def getUsers():
     reg = mongo.db.user_details
-    output = []
-    data = reg.find()
-    for d in data:
-        output.append(
-            {
-                "sellerId": d['sellerId'],
-                "firstName": d['firstName'],
-                "lastName": d['lastName'],
-                "email": d['email'],
-                "phoneNo": d['phoneNo'],
-                "pwd": d['pwd'],
-                "confirmPwd": d['confirmPwd'],
-            }
-        )
-
-    return jsonify({"output": output})
-
-#get one user with input as email
-
-@app.route('/api/getuser/<email>', methods=['GET'])
-def getUser(email):
-    reg = mongo.db.user_details
-    user = reg.find_one({'email': email})
-    if user:
-        output = (
-            {
-                "sellerId": user['sellerId'],
-                "firstName": user['firstName'],
-                "lastName": user['lastName'],
-                "email": user['email'],
-                "phoneNo": user['phoneNo'],
-                "pwd": user['pwd'],
-                "confirmPwd": user['confirmPwd'],
-            }
-        )
+    if reg.query.filter(reg.email == emailid).count() > 0:
+        output = "The provided E-mail is already in use by another user."
+        return jsonify({"result": output})
     else:
-        output = "No Result Found"
+    _hashed_pwd = generate_password_hash(pwd)
+    _hashed_confirmPwd = generate_password_hash(confirmPwd)
+    user = reg.insert({
+        'sellerId': sellerId,
+        'firstName': firstName,
+        'lastName': lastName,
+        'email': emailid,
+        'phoneNo': phoneNo,
+        'pwd': _hashed_pwd,
+        'confirmPwd': _hashed_confirmPwd})
+    db.session.add(user)
+    db.session.commit()
+    data = reg.find_one({"_id": user})
+    return jsonify({"result": data})
 
-    return jsonify({"result": output})
+
+@app.route('/api/user/signin/', methods=[ 'GET'])
+def sign_in():
+    output_data = {'error': ''}
+    if not request.json:
+        output_data['error'] = ERROR_NotJson
+        return jsonify(output_data)
+    emailid = request.json['email']
+    pwd = request.json['pwd']
+    if len(emailid) > 1 :
+       if len(pwd) > 1:
+          result = mongo.db.user_details.query.filter({'email': emailid, 'pwd': check_password_hash(pwd)})
+          if result.count() > 0:
+              if result['disable']=='false':
+                  for doc in result:
+                    doc['_id'] = str(doc['_id'])
+                    output = "Successfully logged in"
+                    output_data['result'] = doc+output
+              else:
+                  output_data = "User Has been Disbled"
+                  return jsonify({"result": output_data})
+
+          else:
+                output_data = "Invalid Login ID or password"
+                return jsonify({"result": output_data})
+
+       else:
+             output_data['error'] = "valid password required"
+
+    else:
+        output_data['error'] = "valid Email-iD required"
+
+    return jsonify(output_data)
 
 
-
-#update profile
-
-@app.route('/api/updateUser/<email>', methods=['GET'])
-def updateUser(email):
-    reg = mongo.db.user_details
-
-    try:
-        firstname = request.json['firstName']
-        lastname = request.json['lastName']
-        emailid = request.json['email']
-        phoneno = request.json['phoneNo']
+@app.route('/api/user/<email>', methods=['GET','UPDATE'])
+def user(email):
+    if request.method == 'GET':
+        signin = mongo.db.user_details
+        user = signin.find_one({'email': email})
+        if user:
+            output = (
+                {
+                    "sellerId": user['sellerId'],
+                    "firstName": user['firstName'],
+                    "lastName": user['lastName'],
+                    "email": user['email'],
+                    "phoneNo": user['phoneNo'],
+                    "pwd": user['pwd'],
+                    "profileImg": user['profileImg'],
+                    "city": user['city'],
+                    "state": user['state'],
+                    "pincode": user['pincode']
+                }
+            )
+        else:
+            output = "No Result Found"
+        return jsonify({"result": output})
+    if request.method == 'UPDATE':
+        firstName = request.json['firstName']
+        lastName = request.json['lastName']
         profileImg = request.json['profileImage']
         city = request.json['city']
         state = request.json['state']
         pincode = request.json['pincode']
 
-        output =reg.update_one(
+        update = mongo.db.user_details
+        output = update.update_one(
             {"email": email},
             {
                 "$set": {
-                    "firstName": firstname,
-                    "lastName": lastname,
-                    "email": emailid,
-                    "phoneNo": phoneno,
+                    "firstName": firstName,
+                    "lastName": lastName,
                     "profileImage": profileImg,
                     "city": city,
                     "state": state,
@@ -110,32 +130,27 @@ def updateUser(email):
             }
         )
         print('\nRecords updated successfully\n')
-    except Exception as e:
-        print(str(e))
-
-    return jsonify({"result": output})
+        return jsonify({"result": output})
 
 
-# insert bank details-------------------
-@app.route('/api/userBankDetails/<sellerId>', methods=['POST'])
+# bank details-------------------
+@app.route('/api/userBankDetails/<sellerId>', methods=['POST','GET'])
 def addBankDetails(sellerId):
     details = mongo.db.bank_details
-
-    try:
-
-        accountNo = request.json['accountNo']
-        accountHolderName = request.json['accountHolderName']
-        bankBranch = request.json['bankBranch']
-        address = request.json['address']
-        state = request.json['state']
-        accountType = request.json['accountType']
-        bankName = request.json['bankName']
-        ifscCode = request.json['ifscCode']
-        city = request.json['city']
-        pincode = request.json['pincode']
-        primaryAccount = request.json['primaryAccount']
-
-        bankDetail=details.insert({
+    if request.method == 'POST':
+        try:
+            accountNo = request.json['accountNo']
+            accountHolderName = request.json['accountHolderName']
+            bankBranch = request.json['bankBranch']
+            address = request.json['address']
+            state = request.json['state']
+            accountType = request.json['accountType']
+            bankName = request.json['bankName']
+            ifscCode = request.json['ifscCode']
+            city = request.json['city']
+            pincode = request.json['pincode']
+            primaryAccount = request.json['primaryAccount']
+            bankDetail=details.insert({
                         "sellerId": sellerId,
                         "accountNo": accountNo,
                         "accountHolderName": accountHolderName,
@@ -148,42 +163,65 @@ def addBankDetails(sellerId):
                         "city": city ,
                         "pincode": pincode,
                         "primaryAccount": primaryAccount
-        })
+            })
+            print('\nRecords updated successfully\n')
+            data = details.find_one({"_id": bankDetail})
+            return jsonify({"result": data})
+        except Exception as e: \
+                print(str(e))
+    else:
+        user = details.find_one({'sellerId': sellerId})
+        if user:
+            output = (
+                {
+                    "sellerId": user['sellerId'],
+                    "accountNo": user['accountNo'],
+                    "accountHolderName": user['accountHolderName'],
+                    "bankBranch": user['bankBranch'],
+                    "address": user['address'],
+                    "state": user['state'],
+                    "accountType": user['accountType'],
+                    "bankName": user['bankName'],
+                    "ifscCode": user['ifscCode'],
+                    "city": user['city'],
+                    "pincode": user['pincode'],
+                    "primaryAccount": user['primaryAccount']
+                }
+            )
+        else:
+            output = "No Result Found"
+        return jsonify({"result": output})
 
-        print('\nRecords updated successfully\n')
-    except Exception as e:
-        print(str(e))
-
-    return jsonify({"result": bankDetail})
+ # add company details-----
 
 
-# add company details-----
-@app.route('/api/userCompanyDetails/<sellerId>', methods=['POST'])
-def addCompanyDetails(sellerId):
-    details = mongo.db.company_details
+# companyDetails-----
+@app.route('/api/userCompanyDetails/<sellerId>', methods=['POST','GET'])
+def companyDetails(sellerId):
+        details = mongo.db.company_details
+        if request.method == 'POST':
+            try:
+                companyId = request.json['companyId']
+                companyName = request.json['companyName']
+                displayName = request.json['displayName']
+                companyPhoneNo = request.json['companyPhoneNo']
+                yearOfExperience = request.json['yearOfExperience']
+                companyEmail = request.json['companyEmail']
+                city = request.json['city']
+                state = request.json['state']
+                pincode = request.json['pincode']
+                tanNo = request.json['tanNo']
+                tradingType = request.json['tradingType']
+                natureOfTrading = request.json['natureOfTrading']
+                gst = request.json['gst']
+                description = request.json['description']
+                companyLogo = request.json['companyLogo']
+                panNo = request.json['panNo']
+                status = request.json['status']
+                remarks = request.json['remarks']
+                dateOfCreation = request.json['dateOfCreation']
 
-    try:
-        companyId = request.json['companyId']
-        companyName = request.json['companyName']
-        displayName = request.json['displayName']
-        companyPhoneNo = request.json['companyPhoneNo']
-        yearOfExperience = request.json['yearOfExperience']
-        companyEmail = request.json['companyEmail']
-        city = request.json['city']
-        state = request.json['state']
-        pincode = request.json['pincode']
-        tanNo = request.json['tanNo']
-        tradingType = request.json['tradingType']
-        natureOfTrading = request.json['natureOfTrading']
-        gst = request.json['gst']
-        description = request.json['description']
-        companyLogo = request.json['companyLogo']
-        panNo = request.json['panNo']
-        status = request.json['status']
-        remarks = request.json['remarks']
-        dateOfCreation = request.json['dateOfCreation']
-
-        company_detail=details.insert({
+                company_detail=details.insert({
                     "sellerId": sellerId,
                     "companyId": companyId,
                     "companyName": companyName,
@@ -204,12 +242,54 @@ def addCompanyDetails(sellerId):
                     "status": status,
                     "remarks": remarks,
                     "dateOfCreation": dateOfCreation
-        })
-        print('\nRecords updated successfully\n')
-    except Exception as e:
-        print(str(e))
+                })
+                print('\nRecords updated successfully\n')
+                data = details.find_one({"_id": company_detail})
+                return jsonify({"result": data})
+            except Exception as e:
+                print(str(e))
+        else:
+            user = details.find_one({'sellerId': sellerId})
+            if user:
+                output = (
+                    {
+                        "sellerId": user['sellerId'],
+                        "companyId": user['companyId'],
+                        "companyName": user['companyName'],
+                        "displayName": user['displayName'],
+                        "companyPhoneNo": user['companyPhoneNo'],
+                        "yearOfExperience": user['yearOfExperience'],
+                        "companyEmail": user['companyEmail'],
+                        "city": user['city'],
+                        "state": user['state'],
+                        "pincode": user['pincode'],
+                        "tanNo": user['tanNo'],
+                        "tradingType": user['tradingType'],
+                        "natureOfTrading": user['natureOfTrading'],
+                        "description": user['description'],
+                        "gst": user['gst'],
+                        "companyLogo": user['companyLogo'],
+                        "panNo": user['panNo'],
+                        "status": user['status'],
+                        "remarks": user['remarks'],
+                        "dateOfCreation": user['dateOfCreation']
+                    }
+                )
+            else:
+                output = "No Result Found"
+            return jsonify({"result": output})
 
-    return jsonify({"result": company_detail})
+
+@app.route('/disableUser')
+def disableUser():
+    reg = mongo.db.user_details
+    _id=reg.insert({
+        'disable': 'true'})
+    session.pop('user', None)
+    data = reg.find_one({"_id": _id})
+    return jsonify({"result": data})
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="localhost", port=5000)
